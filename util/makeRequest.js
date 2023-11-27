f02a2735253d1445cb1f50453388bb8d9c0fe588
TJ Klimoski <tjklimoski@gmail.com>
2023-11-27 18:17:29 -0500

    Create a makeRequest function to handle api calls to server

--- util/makeRequest.js
+++ util/makeRequest.js
@@ -1,5 +1,18 @@
 import axios from "axios";
 
+const API_VERSION = "v1";
+
 const api = axios.create({
-  baseURL: 
-})
+  baseURL: `${process.env.NEXT_PUBLIC_ORIGIN}/api/${API_VERSION}`,
+  withCredentials: true,
+});
+
+export async function makeRequest(path, options) {
+  return api(path, options)
+    .then(res => res.data)
+    .catch(err =>
+      Promise.reject(
+        err?.response?.data?.message ?? err?.response?.data ?? "Error"
+      )
+    );
+}
