21cbab2bce83896b2447104f099bcde83839bf8b
TJ Klimoski <tjklimoski@gmail.com>
2023-11-26 19:50:50 -0500

    Handle a DELETE request to delete a comment from a post

--- app/api/v1/posts/[postId]/comments/[commentId]/route.js
+++ app/api/v1/posts/[postId]/comments/[commentId]/route.js
@@ -37,3 +37,34 @@ export async function PATCH(req, { params }) {
     return new res(err.message ?? "Unknown error", { status: 400 });
   }
 }
+
+// DELETE a specific comment's message on the requested post
+export async function DELETE(req, { params }) {
+  const { commentId } = params;
+  const userId = req.cookies.get("userId")?.value;
+
+  try {
+    // check conditions
+    if (!userId) throw new Error("No UserId");
+
+    // validate the user who's making the edit is the one who created the comment
+    const { userId: commentUserId } = await prisma.comment.findUnique({
+      where: { id: commentId },
+      select: { userId: true },
+    });
+    if (commentUserId !== userId) throw new Error("You do not have permission");
+
+    const comment = await prisma.comment.delete({
+      where: {
+        id: commentId,
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    return res.json(comment);
+  } catch (err) {
+    return new res(err.message ?? "Unknown error", { status: 400 });
+  }
+}
