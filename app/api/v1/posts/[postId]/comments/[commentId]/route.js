14b210c915dd0357f0e4e8d11ac4609cc6a8a154
TJ Klimoski <tjklimoski@gmail.com>
2023-11-26 19:46:22 -0500

    Handle a PATCH request to update a comment's message

---
+++ app/api/v1/posts/[postId]/comments/[commentId]/route.js
@@ -0,0 +1,39 @@
+import { NextResponse as res } from "next/server";
+import prisma from "@/prisma/client";
+
+// UPDATE a specific comment's message on the requested post
+export async function PATCH(req, { params }) {
+  const { message } = await req.json();
+  const { commentId } = params;
+  const userId = req.cookies.get("userId")?.value;
+
+  try {
+    // check conditions
+    if (message === "" || message == null)
+      throw new Error("Message is required");
+    if (!userId) throw new Error("No UserId");
+
+    // validate the user who's making the edit is the one who created the comment
+    const { userId: commentUserId } = await prisma.comment.findUnique({
+      where: { id: commentId },
+      select: { userId: true },
+    });
+    if (commentUserId !== userId) throw new Error("You do not have permission");
+
+    const comment = await prisma.comment.update({
+      where: {
+        id: commentId,
+      },
+      data: {
+        message,
+      },
+      select: {
+        message: true,
+      },
+    });
+
+    return res.json(comment);
+  } catch (err) {
+    return new res(err.message ?? "Unknown error", { status: 400 });
+  }
+}
