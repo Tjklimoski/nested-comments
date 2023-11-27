e85240b61a276a546c27a8523de849631e0c3ee9
TJ Klimoski <tjklimoski@gmail.com>
2023-11-27 17:36:04 -0500

    Add a containing div to the base layout of the app

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={montserrat.className}>{children}</body>
+      <body className={montserrat.className}>
+        <div className="container">{children}</div>
+      </body>
     </html>
   );
 }
