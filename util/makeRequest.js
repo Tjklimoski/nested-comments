97458513362b09a96304743bb2db6f39625b9e22
TJ Klimoski <tjklimoski@gmail.com>
2023-11-27 20:09:29 -0500

    Modify baseUrl

--- util/makeRequest.js
+++ util/makeRequest.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_VERSION = "v1";
 
 const api = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_ORIGIN}/api/${API_VERSION}`,
+  baseURL: `/api/${API_VERSION}`,
   withCredentials: true,
 });
 
