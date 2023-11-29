4f59fd4e349e93291252913677e0aab4130880cd
TJ Klimoski <tjklimoski@gmail.com>
2023-11-29 13:26:14 -0500

    Setup PostProvider to fetch post data and setup comments

--- contexts/PostContext.jsx
+++ contexts/PostContext.jsx
@@ -1,7 +1,9 @@
 "use client";
 
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useMemo } from "react";
 import { useParams } from "next/navigation";
+import { useAsync } from "@/hooks/useAsync";
+import { getPost } from "@/util/posts";
 
 const PostContext = createContext();
 
@@ -11,6 +13,26 @@ export function usePost() {
 
 export function PostProvider({ children }) {
   const { id } = useParams();
+  // Get the post data for the id (using useAsync hook and request func)
+  const { loading, error, value: post } = useAsync(() => getPost(id), [id]);
+  const [comments, setComments] = useState([]);
+  // Set comments from the post fetched
+  useEffect(() => {
+    if (post?.comment == null) return;
+    setComments(post.comment);
+  }, [post?.comment]);
+
+  const commentsByParentId = useMemo(() => {
+    // create an object that will contain parentId fields, with arrays of it's children
+    const parentGroups = {};
+    comments.forEach(comment => {
+      // Set the field to an empty array if it doesn't exisit
+      parentGroups[comment.parentId] ||= [];
+      // Push the child comment to the array
+      parentGroups[comment.parentId].push(comment);
+    });
+    return parentGroups;
+  }, [comments]);
 
   const postContextValues = { id };
 
