e786f90c3815ab2835488136a7d723bd13d22bae
TJ Klimoski <tjklimoski@gmail.com>
2023-11-29 16:54:10 -0500

    Remove usePost from post page

--- app/posts/[id]/page.jsx
+++ app/posts/[id]/page.jsx
@@ -2,11 +2,8 @@
 
 import Post from "@/components/Post";
 import CommentSection from "@/components/CommentSection/CommentSection";
-import { usePost } from "@/contexts/PostContext";
 
 export default function PostPage() {
-  const { post } = usePost();
-
   return (
     <>
       <Post />
