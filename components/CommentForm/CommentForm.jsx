5d9cc36bcd79dd42fdff0a53189b673f66e5a4d4
TJ Klimoski <tjklimoski@gmail.com>
2023-11-29 17:04:18 -0500

    Layout html for CommentForm

---
+++ components/CommentForm/CommentForm.jsx
@@ -0,0 +1,11 @@
+export default function CommentForm() {
+  return (
+    <form>
+      <div>
+        <textarea />
+        <button>POST</button>
+      </div>
+      <div>error message</div>
+    </form>
+  );
+}
