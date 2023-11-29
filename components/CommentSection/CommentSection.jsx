e04a9d8e84fe9898adc9cbfd2f9797645f25fb67
TJ Klimoski <tjklimoski@gmail.com>
2023-11-29 18:47:51 -0500

    Create function to handle user onSubmit in CommentForm component

--- components/CommentSection/CommentSection.jsx
+++ components/CommentSection/CommentSection.jsx
@@ -1,10 +1,36 @@
+import { usePost } from "@/contexts/PostContext";
+import CommentForm from "../CommentForm/CommentForm";
 import styles from "./commentSection.module.css";
+import { useAsyncFn } from "@/hooks/useAsync";
+import { createComment } from "@/util/comments";
 
 export default function CommentSection() {
+  const { post, rootComments, createLocalComment } = usePost();
+  // Get a function we can call to send a POST request to our API
+  const {
+    loading,
+    error,
+    execute: createCommentFn,
+  } = useAsyncFn(createComment);
+
+  function onCreateComment(message) {
+    // called when user submits comment
+    return createCommentFn({ postId: post.id, message }).then(comment =>
+      createLocalComment(comment)
+    );
+  }
+
   return (
     <>
       <h2 className={styles.commentsTitle}>Comments</h2>
-      <section>input form, comment list</section>
+      <section>
+        <CommentForm
+          loading={loading}
+          error={error}
+          onSubmit={onCreateComment}
+        />{" "}
+        comment list
+      </section>
     </>
   );
 }
