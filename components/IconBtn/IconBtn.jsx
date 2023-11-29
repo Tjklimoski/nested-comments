cbfd8e826e0cb0abaf3c48aaf8b68c6c621ecf1c
TJ Klimoski <tjklimoski@gmail.com>
2023-11-29 16:26:01 -0500

    Check status of color when applying class names

--- components/IconBtn/IconBtn.jsx
+++ components/IconBtn/IconBtn.jsx
@@ -4,9 +4,9 @@ import styles from "./iconBtn.module.css";
 export default function IconBtn({ Icon, isActive, color, children, ...props }) {
   return (
     <button
-      className={`${styles.btn} ${styles.iconBtn} ${color || ""} ${
-        isActive ? styles.iconBtnActive : ""
-      }`}
+      className={`${styles.btn} ${styles.iconBtn} ${
+        color === "danger" ? styles.danger : ""
+      } ${isActive ? styles.iconBtnActive : ""}`}
       {...props}
     >
       {/* If children passed, add a margin between the icon and the children */}
