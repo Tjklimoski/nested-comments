d1ddbbcaa1a86f19e3bd367cf0118d4bbfe8e18d
TJ Klimoski <tjklimoski@gmail.com>
2023-11-29 15:35:07 -0500

    Add module.css styles to IconBtn component

---
+++ components/IconBtn/IconBtn.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./iconBtn.module.css";
+
+export default function IconBtn({ Icon, isActive, color, children, ...props }) {
+  return (
+    <button
+      className={`${styles.btn} ${styles.iconBtn} ${color || ""} ${
+        isActive ? styles.iconBtnActive : ""
+      }`}
+      {...props}
+    >
+      {/* If children passed, add a margin between the icon and the children */}
+      <span className={`${children ? "mr-1" : ""}`}>
+        <Icon />
+      </span>
+      {children}
+    </button>
+  );
+}
