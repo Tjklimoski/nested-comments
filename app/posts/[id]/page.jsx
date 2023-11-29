576a0df2fc1b3a2bb6dec6263a701707da6f84ae
TJ Klimoski <tjklimoski@gmail.com>
2023-11-29 12:55:46 -0500

    Use post context on posts page

--- app/posts/[id]/page.jsx
+++ app/posts/[id]/page.jsx
@@ -1,4 +1,9 @@
-export default function PostPage({ params }) {
-  const { id } = params;
+"use client";
+
+import { usePost } from "@/contexts/PostContext";
+
+export default function PostPage() {
+  const { id } = usePost();
+
   return <div>You&#39;re on post page {id}</div>;
 }
