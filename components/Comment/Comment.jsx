d2caec294863cc94d47f184692d1024f71b51f37
TJ Klimoski <tjklimoski@gmail.com>
2023-11-29 20:05:51 -0500

    Create date and time formatter

--- components/Comment/Comment.jsx
+++ components/Comment/Comment.jsx
@@ -1,4 +1,8 @@
-import React from "react";
+// to format the date and time the post was created. undefined use's user's timezone/locale.
+const dateFormatter = new Intl.DateTimeFormat(undefined, {
+  dateStyle: "short",
+  timeStyle: "short",
+});
 
 export default function Comment({ commentData }) {
   const { id, message, user, createdAt, likeCount, likedByMe } = commentData;
