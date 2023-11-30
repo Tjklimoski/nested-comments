f34a08e6ba7e9ab7981372db9f88d2339f1bc9a3
TJ Klimoski <tjklimoski@gmail.com>
2023-11-29 22:19:16 -0500

    Create and style CommentList component

--- components/CommentList/CommentList.jsx
+++ components/CommentList/CommentList.jsx
@@ -1,3 +1,13 @@
-export default function CommentList() {
-  return <div>CommentList</div>;
+import Comment from "../Comment/Comment";
+import styles from "./commentList.module.css";
+
+// Takes an array of comments at the same depth (same parent) and wraps them in a comment stack
+export default function CommentList({ comments }) {
+  return comments.map(comment => {
+    return (
+      <div key={comment.id} className={styles.commentStack}>
+        <Comment commentData={comment} />
+      </div>
+    );
+  });
 }
