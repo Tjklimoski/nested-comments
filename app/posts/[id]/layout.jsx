04131548d22f92aeff4fad44221f5df19e32d8c7
TJ Klimoski <tjklimoski@gmail.com>
2023-12-01 17:23:32 -0500

    Add note to Post pages letting user know the comments reset daily

--- app/posts/[id]/layout.jsx
+++ app/posts/[id]/layout.jsx
@@ -2,5 +2,12 @@ import { PostProvider } from "@/contexts/PostContext";
 import React from "react";
 
 export default function PostLayout({ children }) {
-  return <PostProvider>{children}</PostProvider>;
+  return (
+    <>
+      <p style={{ color: "var(--success)", fontWeight: "bold" }}>
+        Note: Comments reset daily, interact all you want!
+      </p>
+      <PostProvider>{children}</PostProvider>
+    </>
+  );
 }
