b0658b4566fe215cdbfd0087f97324d66cf5f287
TJ Klimoski <tjklimoski@gmail.com>
2023-12-02 15:42:30 -0500

    modify response from /cron/test

--- app/api/v1/cron/test/route.js
+++ app/api/v1/cron/test/route.js
@@ -1,15 +1,6 @@
 import { NextResponse } from "next/server";
 export function GET(request) {
-  console.log("/api/v1/cron/test RAN");
-  return NextResponse.json(
-    {
-      body: request.body,
-      path: request.nextUrl.pathname,
-      query: request.nextUrl.search,
-      cookies: request.cookies.getAll(),
-    },
-    {
-      status: 200,
-    }
-  );
+  return new NextResponse("success", {
+    status: 200,
+  });
 }
